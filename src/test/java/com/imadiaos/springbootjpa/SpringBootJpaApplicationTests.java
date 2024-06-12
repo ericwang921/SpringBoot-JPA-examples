43bf620779494cfa229b8cc831cd1ba890dfe32c
南戈 <wangyanan.0428@163.com>
2024-06-12 18:12:55 +0800

    初始版本

---
+++ src/test/java/com/imadiaos/springbootjpa/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imadiaos.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
